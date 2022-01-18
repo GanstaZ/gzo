870541c8bcc7b22fc2d67f7dcb60ebe25fc1e152
GanstaZ <ganstaz@hotmail.com>
2022-01-18 23:22:10 +0200

    update template files

--- styles/all/template/js/dls.js
+++ styles/all/template/js/dls.js
@@ -2,11 +2,11 @@ $(function() {  // Avoid conflicts with other libraries
 
 'use strict';
 
-$('.tabs .ztab').click(function() {
+$('.c-tabs .c-tab').click(function() {
 	var tab_id = $(this).attr('data-tab');
 
-	$('.tabs .ztab').removeClass('current');
-	$('.tab-content').removeClass('current');
+	$('.c-tabs .c-tab').removeClass('current');
+	$('.c-tab-pane').removeClass('current');
 
 	$(this).addClass('current');
 	$('#'+tab_id).addClass('current');
