ec2610182ed3edddedac0294d9b4154bc1aecc41
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-09-16 21:27:34 +0300

    small update

--- controller/base.php
+++ controller/base.php
@@ -32,19 +32,29 @@ abstract class base
 	/** @var posts */
 	protected $posts;
 
+	/** @var root_path */
+	protected $root_path;
+
+	/** @var php_ext */
+	protected $php_ext;
+
 	/**
 	* Constructor
 	*
-	* @param config   $config   Config object
-	* @param helper	  $helper	Controller helper object
-	* @param language $language Language object
-	* @param posts    $posts    Posts object
+	* @param config   $config    Config object
+	* @param helper	  $helper	 Controller helper object
+	* @param language $language  Language object
+	* @param posts    $posts     Posts object
+	* @param string	  $root_path Path to the phpbb includes directory
+	* @param string	  $php_ext   PHP file extension
 	*/
-	public function __construct(config $config, helper $helper, language $language, posts $posts)
+	public function __construct(config $config, helper $helper, language $language, posts $posts, $root_path, $php_ext)
 	{
-		$this->config   = $config;
-		$this->helper	= $helper;
-		$this->language = $language;
-		$this->posts    = $posts;
+		$this->config    = $config;
+		$this->helper	 = $helper;
+		$this->language  = $language;
+		$this->posts     = $posts;
+		$this->root_path = $root_path;
+		$this->php_ext   = $php_ext;
 	}
 }
