e169cf7f2df200b46e4ec93238ce052b9deb86bb
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-09-17 17:51:54 +0300

    add user service into base controller

--- controller/base.php
+++ controller/base.php
@@ -13,6 +13,7 @@ namespace ganstaz\web\controller;
 use phpbb\config\config;
 use phpbb\controller\helper;
 use phpbb\language\language;
+use phpbb\user;
 use ganstaz\web\model\posts;
 
 /**
@@ -29,6 +30,9 @@ abstract class base
 	/** @var language */
 	protected $language;
 
+	/** @var user */
+	protected $user;
+
 	/** @var posts */
 	protected $posts;
 
@@ -44,15 +48,17 @@ abstract class base
 	* @param config   $config    Config object
 	* @param helper	  $helper	 Controller helper object
 	* @param language $language  Language object
+	* @param user     $user      User object
 	* @param posts    $posts     Posts object
 	* @param string	  $root_path Path to the phpbb includes directory
 	* @param string	  $php_ext   PHP file extension
 	*/
-	public function __construct(config $config, helper $helper, language $language, posts $posts, $root_path, $php_ext)
+	public function __construct(config $config, helper $helper, language $language, user $user, posts $posts, $root_path, $php_ext)
 	{
 		$this->config    = $config;
 		$this->helper	 = $helper;
 		$this->language  = $language;
+		$this->user      = $user;
 		$this->posts     = $posts;
 		$this->root_path = $root_path;
 		$this->php_ext   = $php_ext;
