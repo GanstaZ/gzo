24725afc955935d0c62f7d0bfb13b47cb0e66a96
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-09-27 21:48:43 +0300

    another cs fix

--- controller/forum.php
+++ controller/forum.php
@@ -47,17 +47,17 @@ class forum extends base
 	{
 		$this->main->load();
 
-        $page_title = ($this->config['board_index_text'] !== '') ? $this->config['board_index_text'] : $this->language->lang('INDEX');
+		$page_title = ($this->config['board_index_text'] !== '') ? $this->config['board_index_text'] : $this->language->lang('INDEX');
 
-        /**
-        * You can use this event to modify the page title and load data for the index
-        *
-        * @event core.index_modify_page_title
-        * @var	string	page_title		Title of the index page
-        * @since 3.1.0-a1
-        */
-        $vars = ['page_title'];
-        extract($this->dispatcher->trigger_event('core.index_modify_page_title', compact($vars)));
+		/**
+		* You can use this event to modify the page title and load data for the index
+		*
+		* @event core.index_modify_page_title
+		* @var	string	page_title		Title of the index page
+		* @since 3.1.0-a1
+		*/
+		$vars = ['page_title'];
+		extract($this->dispatcher->trigger_event('core.index_modify_page_title', compact($vars)));
 
 		return $this->helper->render('forum.twig', $this->language->lang($page_title), 200, true);
 	}
