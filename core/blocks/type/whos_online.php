9fde15e87c0d4c7e628f8bf77f03e53cc0848bb4
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-09-27 21:44:41 +0300

    update block

--- core/blocks/type/whos_online.php
+++ core/blocks/type/whos_online.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* GZ Web. An extension for the phpBB Forum Software package.
+* GZO Web. An extension for the phpBB Forum Software package.
 *
 * @copyright (c) 2021, GanstaZ, http://www.github.com/GanstaZ/
 * @license GNU General Public License, version 2 (GPL-2.0)
@@ -10,32 +10,26 @@
 
 namespace ganstaz\web\core\blocks\type;
 
-use phpbb\auth\auth;
-use phpbb\user;
+use ganstaz\web\core\info;
 
 /**
-* GZ Web: Who's Online
+* GZO Web: Who's Online
 */
 class whos_online extends base
 {
-	/** @var auth */
-	protected $auth;
-
-	/** @var user */
-	protected $user;
+	/** @var info */
+	protected $info;
 
 	/**
 	* Constructor
 	*
-	* @param auth $auth Auth object
-	* @param user $user User object
+	* @param info $info Forum info helper object
 	*/
-	public function __construct($config, $db, $controller, $template, $dispatcher, $root_path, $php_ext, auth $auth, user $user)
+	public function __construct($config, $db, $controller, $template, $dispatcher, $root_path, $php_ext, info $info)
 	{
 		parent::__construct($config, $db, $controller, $template, $dispatcher, $root_path, $php_ext);
 
-		$this->auth = $auth;
-		$this->user = $user;
+		$this->info = $info;
 	}
 
 	/**
@@ -65,25 +59,23 @@ class whos_online extends base
 		$users_per_day	= sprintf('%.2f', $total_users / $boarddays);
 
 		// Generate birthday list if required...
-		$show_birthdays = ($this->config['load_birthdays'] && $this->config['allow_birthdays'] && $this->auth->acl_gets('u_viewprofile', 'a_user', 'a_useradd', 'a_userdel'));
-
-		if ($show_birthdays)
+		if ($this->info->show_birthdays())
 		{
-			$this->birthdays();
+			$this->info->birthdays();
 		}
 
-		$this->legend();
+		$this->info->legend();
 
 		$this->template->assign_vars([
-			'gz_posts'	=> $total_posts,
-			'gz_topics' => $total_topics,
-			'gz_users'  => $total_users,
-			'new_user'  => get_username_string('full', (int) $this->config['newest_user_id'], $this->config['newest_username'], $this->config['newest_user_colour']),
+			'TOTAL_POSTS'  => $total_posts,
+			'TOTAL_TOPICS' => $total_topics,
+			'TOTAL_USERS'  => $total_users,
+			'NEWEST_USER'  => get_username_string('full', (int) $this->config['newest_user_id'], $this->config['newest_username'], $this->config['newest_user_colour']),
 
 			'ppd' => $posts_per_day,
 			'tpd' => $topics_per_day,
 			'upd' => $users_per_day,
-			's_birthday_list' => $show_birthdays,
+			'S_DISPLAY_BIRTHDAY_LIST' => $this->info->show_birthdays(),
 		]);
 
 		/**
@@ -95,114 +87,5 @@ class whos_online extends base
 		* @since 2.3.5
 		*/
 		$this->dispatcher->dispatch('ganstaz.web.main_blocks_after');
-	}
-
-	/**
-	* Birthdays
-	*
-	* @return void
-	*/
-	protected function birthdays(): void
-	{
-		$birthdays = [];
-
-		$time = $this->user->create_datetime();
-		$now = phpbb_gmgetdate($time->getTimestamp() + $time->getOffset());
-
-		// Display birthdays of 29th february on 28th february in non-leap-years
-		$leap_year_birthdays = '';
-		if ($now['mday'] == 28 && $now['mon'] == 2 && !$time->format('L'))
-		{
-			$leap_year_birthdays = " OR u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', 29, 2)) . "%'";
-		}
-
-		$sql = $this->db->sql_build_query('SELECT', [
-			'SELECT' => 'u.user_id, u.username, u.user_colour, u.user_birthday',
-			'FROM' => [
-				USERS_TABLE => 'u',
-			],
-			'LEFT_JOIN' => [
-				[
-					'FROM' => [BANLIST_TABLE => 'b'],
-					'ON' => 'u.user_id = b.ban_userid',
-				],
-			],
-			'WHERE' => "(b.ban_id IS NULL OR b.ban_exclude = 1)
-				AND (u.user_birthday LIKE '" . $this->db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%' $leap_year_birthdays)
-				AND u.user_type IN (" . USER_NORMAL . ', ' . USER_FOUNDER . ')',
-		]);
-
-		$result = $this->db->sql_query($sql);
-		$rows = $this->db->sql_fetchrowset($result);
-		$this->db->sql_freeresult($result);
-
-		foreach ($rows as $row)
-		{
-			$birthday_username = get_username_string('full', (int) $row['user_id'], $row['username'], $row['user_colour']);
-			$birthday_year = (int) substr($row['user_birthday'], -4);
-			$birthday_age = ($birthday_year) ? max(0, $now['year'] - $birthday_year) : '';
-
-			$birthdays[] = [
-				'member' => $birthday_username,
-				'age'	 => $birthday_age,
-			];
-		}
-
-		$this->template->assign_block_vars_array('birthdays', $birthdays);
-	}
-
-	/**
-	* Legend
-	*
-	* @return void
-	*/
-	protected function legend(): void
-	{
-		$order_legend = ($this->config['legend_sort_groupname']) ? 'group_name' : 'group_legend';
-
-		// Grab group details for legend display
-		$sql = 'SELECT g.group_id, g.group_name, g.group_colour, g.group_type, g.group_legend
-			FROM ' . GROUPS_TABLE . ' g
-			LEFT JOIN ' . USER_GROUP_TABLE . ' ug
-				ON (
-					g.group_id = ug.group_id
-					AND ug.user_id = ' . (int) $this->user->data['user_id'] . '
-					AND ug.user_pending = 0
-				)
-			WHERE g.group_legend > 0
-				AND (g.group_type <> ' . GROUP_HIDDEN . ' OR ug.user_id = ' . (int) $this->user->data['user_id'] . ')
-			ORDER BY g.' . $order_legend;
-
-		if ($this->auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel'))
-		{
-			$sql = 'SELECT group_id, group_name, group_colour, group_type, group_legend
-				FROM ' . GROUPS_TABLE . '
-				WHERE group_legend > 0
-				ORDER BY ' . $order_legend;
-		}
-		$result = $this->db->sql_query($sql);
-
-		$legend = [];
-		while ($row = $this->db->sql_fetchrow($result))
-		{
-			$this->template->assign_block_vars('legend', [
-				'color' => (string) $row['group_colour'],
-				'name'	=> (string) $row['group_name'],
-				'link'	=> (string) append_sid("{$this->get('root_path')}memberlist.{$this->get('php_ext')}", "mode=group&amp;g={$row['group_id']}"),
-				'not_authed' => (bool) $this->not_authed($row),
-			]);
-		}
-		$this->db->sql_freeresult($result);
-	}
-
-	/**
-	* Is visitor a bot or does he/she have permissions
-	*
-	* @param array $row Groups data
-	* @return bool
-	*/
-	protected function not_authed($row): bool
-	{
-		return $row['group_name'] == 'BOTS' || ($this->user->data['user_id'] != ANONYMOUS && !$this->auth->acl_get('u_viewprofile'));
 	}
 }
