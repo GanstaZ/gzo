24705f41513e760c2060aa2ed610783bface217a
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2022-10-07 18:16:31 +0300

    update core tabs

--- core/tabs/type/base.php
+++ core/tabs/type/base.php
@@ -3,7 +3,7 @@
 *
 * GZO Web. An extension for the phpBB Forum Software package.
 *
-* @copyright (c) 2021, GanstaZ, http://www.github.com/GanstaZ/
+* @copyright (c) 2022, GanstaZ, http://www.github.com/GanstaZ/
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
@@ -19,7 +19,7 @@ use phpbb\template\template;
 use phpbb\exception\http_exception;
 
 /**
-* GZO Web: Member profile model
+* GZO Web: base class for tabs
 */
 class base implements tabs_interface
 {
