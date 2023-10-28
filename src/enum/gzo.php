7ea190fd2e802f346e44d9f41dc2b3d287b04a86
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2023-10-28 19:55:18 +0300

    add main constant class

---
+++ src/enum/gzo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+*
+* An extension for the phpBB Forum Software package.
+*
+* @copyright (c) GanstaZ, https://www.github.com/GanstaZ/
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace ganstaz\gzo\src\enum;
+
+/**
+* GZO
+*/
+final class gzo
+{
+	public const VERSION = '3.0.0-dev';
+	public const DATE_FORMAT = 'Y-m-d H:i';
+	public const MAIN_MIGRATION = '\ganstaz\gzo\migrations\v24\m1_main';
+	// public const MAIN_MIGRATION = '\ganstaz\gzo\migrations\v30\m1_main';
+}
