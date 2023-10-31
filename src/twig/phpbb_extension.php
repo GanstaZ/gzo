e8c549b52b483355688e8c6f75d3ce701a260b94
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2023-10-31 20:38:03 +0200

    Fix cs error

--- src/twig/phpbb_extension.php
+++ src/twig/phpbb_extension.php
@@ -10,23 +10,10 @@
 
 namespace ganstaz\gzo\src\twig;
 
-use phpbb\template\context;
-use phpbb\template\twig\environment;
-use phpbb\language\language;
-
 use phpbb\template\twig\extension;
 
 class phpbb_extension extends extension
 {
-	public function __construct(
-		context $context,
-		environment $environment,
-		language $language
-	)
-	{
-		parent::__construct($context, $environment, $language);
-	}
-
 	/**
 	* Returns the token parser instance to add to the existing list.
 	*/
