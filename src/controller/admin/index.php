3ad250e49aea9a814a6d0714b003b1dec059aacb
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2025-01-23 18:52:16 +0200

    Use constant instead of config var

--- src/controller/admin/index.php
+++ src/controller/admin/index.php
@@ -11,31 +11,12 @@
 namespace ganstaz\gzo\src\controller\admin;
 
 use ganstaz\gzo\src\controller\abstract_controller;
-use ganstaz\gzo\src\controller\helper;
-use ganstaz\gzo\src\entity\manager as em;
 use ganstaz\gzo\src\enum\gzo;
-use ganstaz\gzo\src\form\form;
-
-use phpbb\config\config;
-use phpbb\event\dispatcher;
 use Symfony\Component\HttpFoundation\Response;
 
 #[\ganstaz\gzo\src\attribute\auth('ROLE_ADMIN', 'a_board', 'GZO_NO_ADMIN', 403)]
 class index extends abstract_controller
 {
-	public function __construct(
-		dispatcher $dispatcher,
-		helper $helper,
-		em $em,
-		form $form,
-		$root_path,
-		$php_ext,
-		private readonly config $config
-	)
-	{
-		parent::__construct($dispatcher, $helper, $em, $form, $root_path, $php_ext);
-	}
-
 	public function main(): Response
 	{
 		$this->helper->language->add_lang('area_gzo', 'ganstaz/gzo');
@@ -46,7 +27,7 @@ class index extends abstract_controller
 			'GZO_STYLE'        => gzo::STYLE,
 
 			'PHP_VERSION_INFO' => PHP_VERSION,
-			'BOARD_VERSION'    => $this->config['version'],
+			'BOARD_VERSION'    => PHPBB_VERSION,
 		]);
 
 		return $this->helper->controller_helper->render('admin/index.twig', $this->helper->language->lang('GZO_MAIN_PAGE'));
